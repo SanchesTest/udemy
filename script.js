3dabc0b20a1fafd31f53d1ecfb576d9e5ff67be3
Alexandr <saanches7@ukr.net>
2019-12-03 20:07:43 +0200

    third commit

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let num = 50;
+
+switch(num){
+  case num < 49:
+      console.log('false');
+      break;
+  case num > 100:
+      console.log('false');
+      break;
+  case 50:
+      console.log('true');
+      break;
+  default:
+      console.log('problem');
+      break;
+}
